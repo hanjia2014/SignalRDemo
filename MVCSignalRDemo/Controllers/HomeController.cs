9f203b8263694a3ded8d214ecef49b7d07ea543c
hanjia2014 <hanj@datacom.co.nz>
2015-07-10 14:38:32 +1200

    created

---
+++ MVCSignalRDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.SignalR;
+using MVCSignalRDemo.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCSignalRDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: /Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Name,RollNo")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                var context = GlobalHost.ConnectionManager.GetHubContext<StudentChange>();
+                context.Clients.All.Poke("update");
+                //return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCSignalRDemo/Models/StudentModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCSignalRDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RollNo { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
